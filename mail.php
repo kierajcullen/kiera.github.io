c9ced3a3e13247a8e13f3765605bf8a8ed7b8e71
kierajcullen <kierajcullen@gmail.com>
2020-12-16 19:54:02 -0500

    testing 1..2...3

--- mail.php
+++ mail.php
@@ -14,3 +14,34 @@ $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $content, $mailheader) or die("Error!");
 echo "Email sent!";
 ?>
+
+
+<?php
+$name = $_POST['name'];
+$subject = $_POST['subject'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+
+$to = "benmuschol@gmail.com";
+$subject = "Hello World";
+$message = "Name: $name \n Subject: $subject \n Email: $email \n Message: $message \n";
+
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+// Additional headers
+$headers .= 'From: '.$email. "\r\n";
+
+
+if(mail($to, $subject, $message, $headers))
+{
+    echo "Mail Sent Successfully.";
+}
+else
+{
+    echo "Error in Mail Sent.";
+}
+
+
+?>
